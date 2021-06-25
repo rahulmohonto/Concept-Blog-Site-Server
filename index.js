7d70937402bb500162155086cb933efbd2e58738
rahulmohonto <rahulmohonto1999@gmail.com>
2021-06-26 04:58:16 +0600

    testing

--- index.js
+++ index.js
@@ -59,29 +59,29 @@ client.connect(err => {
 
 
 
-    app.post('/allBlogs', (req, res) => {
-        const blogData = req.body;
-        const email = req.body.email
-        console.log(blogData);
-        adminCollection.find({ email: email })
-            .toArray(err, admin => {
-                const filter = { email: email }
-                if (admin.length === 0) {
-                    filter.email = email;
-                }
+    // app.post('/allBlogs', (req, res) => {
+    //     const blogData = req.body;
+    //     const email = req.body.email
+    //     console.log(blogData);
+    //     adminCollection.find({ email: email })
+    //         .toArray(err, admin => {
+    //             const filter = { email: email }
+    //             if (admin.length === 0) {
+    //                 filter.email = email;
+    //             }
 
-                blogCollection.find(filter)
-                    .toArray(err, documents => {
-                        res.send(documents);
-                    })
-            })
+    //             blogCollection.find(filter)
+    //                 .toArray(err, documents => {
+    //                     res.send(documents);
+    //                 })
+    //         })
 
-    })
+    // })
 
 
     app.get('/isAdmin', (req, res) => {
-        // const email = req.body.email;
-        adminCollection.find({})
+        const email = req.body.email;
+        adminCollection.find({ email: email })
             .toArray((error, documents) => {
                 res.send(documents)
             })
